6af779cdf3e521edf2783b147a2be3142249aa5b
C0deWeather <bflloyd1653@gmail.com>
2024-03-18 15:25:06 -0700

    modify a copy of a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """
+    Replaces an element at idx in a list.
+
+    Args:
+    my_list (list): the list
+    idx (int): index of element in list
+    element: the replacement
+
+    Returns:
+    list: a new list containing the replacement
+    """
+
+    new_list = my_list.copy()
+
+    if idx > len(my_list) or idx < 0:
+        return new_list
+
+    new_list[idx] = element
+    return new_list
