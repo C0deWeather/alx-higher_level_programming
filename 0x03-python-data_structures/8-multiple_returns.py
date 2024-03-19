afa68b8f788f3342a4581cdc022bd013a0405334
C0deWeather <bflloyd1653@gmail.com>
2024-03-19 05:28:43 -0700

    more returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,13 +2,13 @@
 def multiple_returns(sentence):
     """
     Returns a tuple with the length of a string and its first character
-    
+
     Args:
     sentence (str): the string
 
     Returns:
     tuple: containing the length of the string and its first character.
     """
-    if sentence == ():
+    if sentence == "":
         return (0, none)
     return (len(sentence), sentence[0])
