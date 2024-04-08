9c2e69db88e42a793872df9ab885d6bb5b686986
C0deWeather <bflloyd1653@gmail.com>
2024-04-08 16:34:43 -0700

    using getters and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@
 class Rectangle:
     """This class represents a rectangle"""
 
-    def __init(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """Creates an instance of the class"""
         self.__width = width
         self.__height = height
@@ -20,7 +20,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the value of width"""
-        if value is not type(int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -35,7 +35,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the value of height"""
-        if value is not type(int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
