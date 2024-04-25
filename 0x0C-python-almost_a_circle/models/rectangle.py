face7ddcad3ef2cacc48aa632be742ade5bc2814
C0deWeather <bflloyd1653@gmail.com>
2024-04-25 14:55:35 -0700

    validated setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Computes area of rectangle"""
+        return self.width * self.height
