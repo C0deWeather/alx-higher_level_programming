e8b901b46948455b3af894d4506cc100bf6a5732
C0deWeather <frankbright211@gmail.com>
2024-06-20 09:39:04 +0100

    arguments found

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (argv.slice(2).length > 1) {
+	console.log('Arguments found');
+}
+else if (argv.slice(2).length == 1) {
+	console.log('Argument found');
+}
+else {
+	console.log('No argument');
+}
