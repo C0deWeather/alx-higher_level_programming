9690d43ea1fbf20a324bd2183a8c46d3a85f7d11
C0deWeather <frankbright211@gmail.com>
2024-06-24 09:28:33 +0100

    My first experience with mysqldb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    try:
+        db_obj = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            password=password,
+            database=db_name)
+        cur = db_obj.cursor()
+        cur.execute("select * from states")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        pass
+    cur.close()
+    db_obj.close()
+
+
+if __name__ == "__main__":
+    main()
