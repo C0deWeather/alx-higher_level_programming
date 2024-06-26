fa5fa9e31e29176b5474bd6f106413b066bc32b5
C0deWeather <frankbright211@gmail.com>
2024-06-26 01:05:13 +0100

    first state model with sqlalchemy orm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module declares an instance of Declarative and also
+defines a class that inherits from the declarative base class"""
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String
 
