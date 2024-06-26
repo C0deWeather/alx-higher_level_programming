f6b104bf3a5e3933cff787a6bf6b1cc6cb3afce5
C0deWeather <frankbright211@gmail.com>
2024-06-26 01:02:08 +0100

    first state model with sqlalchemy orm

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This class represents a table in a mysql database"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
