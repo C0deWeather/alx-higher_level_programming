006496d53b3d31e919c08d1dcc047a928a03a4d6
C0deWeather <frankbright211@gmail.com>
2024-07-05 20:51:31 +0100

    script to retrive the value of the X-Request-Id variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,12 +2,9 @@
 """This script takes in a URL, sends a request to it, and displays the value
 of the X-Request-Id variable found in the header of the response"""
 
-
-
 if __name__ == '__main__':
     import sys
     import urllib.request
-
 
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
