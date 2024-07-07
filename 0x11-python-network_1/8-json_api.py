2c4103a7cea3a1cba4a7ae44c40647c39f381627
C0deWeather <frankbright211@gmail.com>
2024-07-07 21:46:55 +0100

    json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script takes in a letter and sends a post request to a given
+url with the letter as a parameter"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 2 else ""
+    payload = {'q': q}
+    response = requests.post(url, data=payload)
+    try:
+        data = response.json()
+        if data:
+            print(f"[{data.get('id')}] {data.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
